d903b272a82e32e47b1b8e1842b7a026868a9d02
Patric Steffen <patric.steffen@1und1.de>
2020-08-05 21:01:43 +0200

    Updated batman version to an own build version removed multiple time consuming apt cache updates updated deprecated bat -m commands

--- roles/install_fastd/templates/fastd_up.sh
+++ roles/install_fastd/templates/fastd_up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-batctl -m bat{{ site.name }} if add vpn{{ site.name }}
+batctl meshif bat{{ site.name }} if add vpn{{ site.name }}
 ip link set address {{ all_sites[site.name].fastd.mesh_mac[site.node_number-1]}} dev vpn{{ site.name }}
 ip link set up dev $1
-batctl -m bat{{ site.name }} gw server 1000000/1000000
-batctl -m bat{{ site.name }} it 10000
-batctl -m bat{{ site.name }} mm 1
+batctl meshif bat{{ site.name }} gw server 1000000/1000000
+batctl meshif bat{{ site.name }} it 10000
+batctl meshif bat{{ site.name }} mm 1
 echo 64 > /sys/class/net/bat{{ site.name }}/mesh/hop_penalty
 #netctl start bat{{ site.name }}
 #ifdown bat{{ site.name }}
--- roles/install_wireguard_mesh/templates/up.sh
+++ roles/install_wireguard_mesh/templates/up.sh
@@ -5,12 +5,12 @@ ip link add mesh{{ site.name }}{{ remote_site.node_number }} type ip6gretap remo
 ip link set mtu 1312 dev mesh{{ site.name }}{{ remote_site.node_number }}
 ip link set address {{ all_sites[remote_site.name].wireguard_mesh.mac_prefix[site.node_number-1] }}{{ remote_site.node_number }} dev mesh{{ site.name }}{{ remote_site.node_number }}
 ip link set up dev mesh{{ site.name }}{{ remote_site.node_number }}
-batctl -m bat{{ site.name }} if add mesh{{ site.name }}{{ remote_site.node_number }}
+batctl meshif bat{{ site.name }} if add mesh{{ site.name }}{{ remote_site.node_number }}
 {% endfor %}
 {% endfor %}
-batctl -m bat{{ site.name }} gw server 1000000/1000000
-batctl -m bat{{ site.name }} it 10000
-batctl -m bat{{ site.name }} mm 1
+batctl meshif bat{{ site.name }} gw server 1000000/1000000
+batctl meshif bat{{ site.name }} it 10000
+batctl meshif bat{{ site.name }} mm 1
 echo 64 > /sys/class/net/bat{{ site.name }}/mesh/hop_penalty
 ifup bat{{ site.name }}
 #systemctl restart isc-dhcp-server.service
