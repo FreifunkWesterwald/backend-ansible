87f3689e645bed3d3f630bd7e9e6f3ef757e9622
loppermann <git@loppermann.de>
2021-07-28 23:53:20 +0200

    change mesh wireguard to vxlan

--- roles/install_wireguard_mesh/templates/up.sh
+++ roles/install_wireguard_mesh/templates/up.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+ip -6 link add vx{{ site.name }} type vxlan id {{ all_sites[site.name].wireguard_mesh.vxlan_id }} dstport 8472 local {{ all_sites[site.name].wireguard_mesh.meshaddress[site.node_number-1] }} dev wg{{ site.name }}
+ip link set mtu 1280 dev vx{{ site.name }}
+ip link set address {{ all_sites[site.name].wireguard_mesh.macaddress[site.node_number-1] }} dev vx{{ site.name }}
+ip link set up dev vx{{ site.name }}
+batctl meshif bat{{ site.name }} if add vx{{ site.name }}
+
+
 {% for host in groups['fastd'] %}
 {% for remote_site in hostvars[host]['sites'] if remote_site.name == site.name and remote_site.node_number != site.node_number %}
-ip link add mesh{{ site.name }}{{ remote_site.node_number }} type ip6gretap remote {{ all_sites[remote_site.name].wireguard_mesh.address[remote_site.node_number-1] }} local {{ all_sites[remote_site.name].wireguard_mesh.address[site.node_number-1] }} ttl 255 dev wg{{ site.name }}
-ip link set mtu 1312 dev mesh{{ site.name }}{{ remote_site.node_number }}
-ip link set address {{ all_sites[remote_site.name].wireguard_mesh.mac_prefix[site.node_number-1] }}{{ remote_site.node_number }} dev mesh{{ site.name }}{{ remote_site.node_number }}
-ip link set up dev mesh{{ site.name }}{{ remote_site.node_number }}
-batctl meshif bat{{ site.name }} if add mesh{{ site.name }}{{ remote_site.node_number }}
+bridge fdb append 00:00:00:00:00:00 dev vx{{ site.name }} dst {{ all_sites[remote_site.name].wireguard_mesh.meshaddress[remote_site.node_number-1] }}
+
 {% endfor %}
 {% endfor %}
 batctl meshif bat{{ site.name }} gw server 1000000/1000000
