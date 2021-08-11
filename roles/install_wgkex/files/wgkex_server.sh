f3c3a4923740c2ef36fe5664cdda500b99445771
Patric Steffen <patric.steffen@1und1.de>
2021-08-11 13:33:41 +0200

    Adjust port for wgkex and use dualstack for listening

--- roles/install_wgkex/files/wgkex_server.sh
+++ roles/install_wgkex/files/wgkex_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-socat "TCP-LISTEN:$LISTEN_PORT,bind=$LISTEN_IP,max-children=$MAX_CHILDREN,fork" \
+socat "TCP6-LISTEN:$LISTEN_PORT,max-children=$MAX_CHILDREN,fork" \
       EXEC:"sudo /usr/local/bin/wgkex_handler.sh"
