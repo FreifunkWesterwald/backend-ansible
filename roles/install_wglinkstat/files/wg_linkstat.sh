6f2b9892aa14c591e7e2b69cf7ea9ac83955d6d1
pmenke <info@pmenke.de>
2021-08-27 17:11:20 +0200

    added wg linkstat on @justelex's grafana

---
+++ roles/install_wglinkstat/files/wg_linkstat.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if ! wg show "$1" 2>&1 >/dev/null; then
+  "invalid interface $1"
+  exit 1
+fi
+while true; do
+  wg show "$1" dump | \
+    tail -n +2 | \
+    cut -d$'\t' -f 4,6,7 | \
+    sed -E 's/(.*)\/[0-9]+\t(.*)\t(.*)/linkstat,peer="\1",gw="'$(hostname)'" rx-bytes=\2,tx-bytes=\3 '$(date +%s)'/' | \
+    curl -s -X POST 'http://freifunk.tools:8086/write?db=ffww&precision=s' --data-binary @-
+  sleep 10
+done
