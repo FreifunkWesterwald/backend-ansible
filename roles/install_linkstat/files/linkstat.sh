299f160836882d1d8efef68f03c000247abb2e0f
pmenke <info@pmenke.de>
2021-08-30 17:58:51 +0200

    changed templating / configuration of influx-db connection properties

--- roles/install_linkstat/files/linkstat.sh
+++ roles/install_linkstat/files/linkstat.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+if [ -z "$INFLUX_USER" ]; then
+  echo "INFLUX_USER not set";
+  exit 1;
+elif [ -z "$INFLUX_PASSWORD" ]; then
+  echo "$INFLUX_PASSWORD not set";
+  exit 1;
+elif [ -z "$INFLUX_BASE_URL" ]; then
+  echo "$INFLUX_BASE_URL not set";
+  exit 1;
+elif [ -z "$INFLUX_DB" ]; then
+  echo "$INFLUX_DB not set";
+  exit 1;
+fi
+
 while true; do
-  for int in /sys/class/net/{bb*,bat*,wg*}; do
+  for int in $(ls -1d /sys/class/net/{bb*,bat*,wg*} 2>/dev/null); do
     echo 'bbstat,interface="'$(basename $int)\
 '",node="'$(hostname)'"'\
 ' '\
@@ -11,6 +25,6 @@ while true; do
 ',tx_packets='$(cat $int/statistics/tx_packets)\
 ' '\
 $(date +%s);
-  done | curl -s -X POST 'http://freifunk.tools:8086/write?db=ffww&precision=s' --data-binary @-
+  done | curl -s -u "$INFLUX_USER:$INFLUX_PASSWORD" -X POST "$INFLUX_BASE_URL/write?db=$INFLUX_DB&precision=s" --data-binary @-
   sleep 10
 done
--- roles/install_wglinkstat/files/wg_linkstat.sh
+++ roles/install_wglinkstat/files/wg_linkstat.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
+
 if ! wg show "$1" 2>&1 >/dev/null; then
-  "invalid interface $1"
+  echo "invalid interface $1"
   exit 1
+elif [ -z "$INFLUX_USER" ]; then
+  echo "INFLUX_USER not set";
+  exit 1;
+elif [ -z "$INFLUX_PASSWORD" ]; then
+  echo "$INFLUX_PASSWORD not set";
+  exit 1;
+elif [ -z "$INFLUX_BASE_URL" ]; then
+  echo "$INFLUX_BASE_URL not set";
+  exit 1;
+elif [ -z "$INFLUX_DB" ]; then
+  echo "$INFLUX_DB not set";
+  exit 1;
 fi
+
 while true; do
   wg show "$1" dump | \
     tail -n +2 | \
     cut -d$'\t' -f 4,6,7 | \
     sed -E 's/(.*)\/[0-9]+\t(.*)\t(.*)/linkstat,peer="\1",gw="'$(hostname)'" rx-bytes=\2,tx-bytes=\3 '$(date +%s)'/' | \
-    curl -s -X POST 'http://freifunk.tools:8086/write?db=ffww&precision=s' --data-binary @-
+    curl -s -u "$INFLUX_USER:$INFLUX_PASSWORD" -X POST "$INFLUX_BASE_URL/write?db=$INFLUX_DB&precision=s" --data-binary @-
   sleep 10
 done
